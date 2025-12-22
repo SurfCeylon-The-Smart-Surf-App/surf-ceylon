369e58927bf923ef173c092013f6997833086095
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-12-22 11:14:03 +0530

    style: format API_HOST declaration for improved readability

--- SurfApp--frontend/utils/networkConfig.js
+++ SurfApp--frontend/utils/networkConfig.js
@@ -3,7 +3,12 @@ import { Platform } from "react-native";
 // Simple network configuration for development
 // For Android Emulator, use 10.0.2.2 to access host machine's localhost
 // For iOS/physical devices, use your WiFi IP address
-const API_HOST = Platform.OS === "android" ? "10.0.2.2" : Platform.OS === "web" ? "localhost" : "172.20.10.2";
+const API_HOST =
+  Platform.OS === "android"
+    ? "10.0.2.2"
+    : Platform.OS === "web"
+    ? "localhost"
+    : "172.20.10.2";
 const API_PORT = 3000;
 
 console.log("Platform.OS:", Platform.OS);
